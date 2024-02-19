8173fd872d7ec0d4178c7d205d036692c9d358f2
Admin <Admin@DESKTOP-7T0EOMO>
2024-02-19 07:52:19 +0530

    member class

---
+++ MemberClass/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberClass
+{
+    class Car
+    {
+        private string CName;
+        public void GetData()
+        {
+            CName = "Car";
+        }
+        public void Display()
+        {
+            Console.WriteLine(CName);
+        }
+        public class Sterio
+        {
+            private string SName;
+            public void GetInfo()
+            {
+                SName = "Sterio";
+            }
+            public void show()
+            {
+                Console.WriteLine(SName);
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c=new Car();
+            Car.Sterio s=new Car.Sterio();
+
+            c.GetData();
+            s.GetInfo();
+
+            c.Display();
+            s.show();
+        }
+    }
+}
